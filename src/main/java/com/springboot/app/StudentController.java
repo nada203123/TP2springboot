b25d0657b444bc0b1579616be8795a0e662f5f07
nada ghribi <nada.ghribi203@gmail.com>
2023-10-12 10:23:24 +0200

    1er commit

---
+++ src/main/java/com/springboot/app/StudentController.java
@@ -0,0 +1,20 @@
+package com.springboot.app;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class StudentController {
+// build rest API to handle query parameters
+//http://localhost:8080/student/query?firstName=Ramesh&lastName=Fadatare
+    @GetMapping("/student/query")
+    public Student studentQueryParam(
+            @RequestParam(name = "firstName") String firstName,
+            @RequestParam(name = "lastName") String lastName) {
+        return new Student(firstName, lastName);
+    }
+
+
+}
